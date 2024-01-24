246641cbc7abb9116d23d4f2b66b082cb361a3f9
SultonbekKhasanboev <sultonbekkhasanboev@gmail.com>
2024-01-24 17:13:54 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+var elInp = document. querySelector(' .inp')
+var elTitle = document. querySelector('.title')
+function fn(){
+    var val = elInp. value
+    if( val % 3 == 0 && val % 7 == 0){
+    elTitle. textContent = 'FIZZBUZZ'
+    }else if(val % 7 == 0){
+    elTitle. textContent = 'BUZZ'
+    }else if(val % 3 == 0){
+       elTitle. textContent = 'FIZZ'
+    }else{
+       elTitle.textContent = 'bolinmaydi '
+    }
+    elInp. value =
+    elInp. focus ()
+    }
